ed32b6d41a2086659c9029a5b55178fe3d122af9
tmdals5718 <tmdals5718@gmail.com>
2019-04-21 16:09:18 +0900

    add

---
+++ unit-test-1week-master/src/main/java/UpperCaseCounterTest.java
@@ -0,0 +1,37 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UpperCaseCounterTest {
+    private UpperCaseCounter upperCaseCounter = new UpperCaseCounter();
+
+    @Test
+    public void 널체크(){
+        String str = null;
+        int result =  upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue(result ==0);
+    }
+    @Test
+    public void 빈값체크(){
+        String str = "";
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue(result==0);
+    }
+    @Test
+    public void 대문자_ABC(){
+        String str = "ABC";
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        System.out.println("result :: "+ result);
+        assertTrue(result==3);
+        assertFalse(result == 2);
+    }
+    @Test
+    public void 대문자_ABCcdefGHI(){
+        String str = "ABCcdefGHI";
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        System.out.println("result :: "+ result);
+        assertTrue(result== 6);
+        assertFalse(result == 5);
+    }
+}
